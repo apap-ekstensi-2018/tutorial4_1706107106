ef685c365124ef7323af8a1f6cacb5c4726e13ca
Yohannes Sakti Panggabean <yoanhans@gmail.com>
2018-03-10 23:51:03 +0700

    Penyelesaian Tutorial 4

---
+++ tutorial4_1706107106/tutorial4/src/main/java/com/example/tutorial4/service/StudentService.java
@@ -0,0 +1,12 @@
+package com.example.tutorial4.service;
+
+import java.util.List;
+import com.example.tutorial4.model.StudentModel;
+
+public interface StudentService {
+	StudentModel selectStudent(String npm);
+	List<StudentModel> selectAllStudents();
+	void addStudent(StudentModel student);
+	boolean deleteStudent(String npm);
+	void updateStudent(StudentModel student);
+}
